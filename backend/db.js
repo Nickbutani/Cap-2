d290c072fb6b463eba2cedb93316ddaf540826f7
nickbutani <naitikbu@gmail.com>
2024-06-27 21:12:24 -0400

    update

--- backend/db.js
+++ backend/db.js
@@ -4,15 +4,26 @@ let DB_URI;
 
 if (process.env.NODE_ENV === 'test') {
   DB_URI = 'postgresql:///greenthumb_test';
-}
-else {
+} else if (process.env.NODE_ENV === 'production') {
+  DB_URI = 'postgres://wglihlii:STPqHtuqMRCaA1vGbig5Yo5kJ3ceMUrI@stampy.db.elephantsql.com/wglihlii';
+} else {
   DB_URI = 'postgresql:///greenthumb';
 }
 
-let db = new Client({
+let dbConfig = {
   connectionString: DB_URI
-});
+};
 
-db.connect();
+if (process.env.NODE_ENV === 'production') {
+  dbConfig.ssl = {
+    rejectUnauthorized: false
+  };
+}
+
+let db = new Client(dbConfig);
+
+db.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Connection error', err.stack));
 
 module.exports = db;
